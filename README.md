83a9d5bb08c058720a7058a6a7776274a7a51c3e
Jon Auer <jda@coldshore.com>
2014-03-12 00:22:32 -0500

    initial commit & things work

---
+++ README.md
@@ -0,0 +1,7 @@
+# ptrsweep
+Get PTR records (reverse DNS) for all IPs in a subnet
+
+Usage: ptrsweep 192.168.0.0/24
+
+## Credits
+Address generation per this thread: https://groups.google.com/forum/#!topic/golang-nuts/zlcYA4qk-94
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Required argument \"NET/MASK\" missing")
+	}
+
+	block := os.Args[1]
+
+	ip, ipnet, err := net.ParseCIDR(block)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); incIP(ip) {
+		wg.Add(1)
+		myip := ip.String()
+		go revIP(myip, wg)
+	}
+	wg.Wait()
+
+}
+
+func revIP(ip string, wg sync.WaitGroup) {
+	defer wg.Done()
+	ptr, _ := net.LookupAddr(ip)
+	fmt.Printf("%s,%s\n", ip, ptr)
+}
+
+func incIP(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
